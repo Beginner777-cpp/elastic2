cc74b4a5794cc2dc03a307c3a8dbd6870a625f04
husan <test>
2021-01-16 19:13:47 +0500

    test

--- app/src/script/common.js
+++ app/src/script/common.js
@@ -2,7 +2,7 @@ class Slider {
     constructor(obj) {
         this.slider = document.querySelector(obj.slider)
         this.pages = this.slider.querySelector('.carousel_pages').children
-        this.indicators = this.slider.querySelector('.indicators_list').children
+        this.indicators = this.slider.querySelectorAll('.indicator')
         this.prevBtn = this.slider.querySelector('.prev_btn')
         this.nextBtn = this.slider.querySelector('.next_btn')
         this.activePage = 0;
@@ -12,33 +12,118 @@ class Slider {
             this.pages[i].style.transform = `translateY(${this.pageHeight}px)`;
         }
         this.pages[this.activePage].style.transform = `translateY(0)`
+        /*======================================================= prevNextBtns ===============================================*/
         this.prevBtn.addEventListener('click', () => {
-            this.move(this.prevBtn)
+            if (this.flag == true) {
+                this.move(this.prevBtn)
+                this.disableBtns(true)
+                setTimeout(() => {
+                    this.disableBtns(false)
+                }, 1000);
+            }
         })
         this.nextBtn.addEventListener('click', () => {
-            this.move(this.nextBtn)
+            if (this.flag == true) {
+                this.move(this.nextBtn)
+                this.disableBtns(true)
+                setTimeout(() => {
+                    this.disableBtns(false)
+                }, 1000);
+            }
+
         })
+        /*======================================================= /prevNextBtns ===============================================*/
+
+        /*======================================================= indicators==================================================*/
 
         for (let i = 0; i < this.indicators.length; i++) {
             this.indicators[i].addEventListener('click', () => {
-                if (i > this.activePage) {
-                    var interval = setInterval(() => {
-                        this.move(this.nextBtn)
-                        if (i == this.activePage) {
-                            clearInterval(interval);
-                        }
-                    }, 1000);
-                } else if (i < this.activePage) {
-                    var interval = setInterval(() => {
-                        this.move(this.prevBtn)
-                        if (i == this.activePage) {
-                            clearInterval(interval);
-                        }
-                    }, 1000);
+                if (this.flag == true) {
+                    if (i > this.activePage) {
+                        this.flag = false;
+                        var interval = setInterval(() => {
+                            this.move(this.nextBtn)
+                            if (i == this.activePage) {
+                                clearInterval(interval);
+                                this.disableIndicators(false)
+                                this.flag = true;
+                            }
+                        }, 500);
+                    } else if (i < this.activePage) {
+                        this.flag = false;
+                        var interval = setInterval(() => {
+                            this.move(this.prevBtn)
+                            if (i == this.activePage) {
+                                clearInterval(interval);
+                                this.disableIndicators(false)
+                                this.disableBtns(true)
+                                this.flag = true
+                            }
+                        }, 500);
+                    }
                 }
+
+
             })
 
         }
+        /*======================================================= /indicators ==================================================*/
+
+
+        /*======================================================= wheel =========================================================*/
+
+        window.addEventListener('wheel', (e) => {
+            if (this.flag == true) {
+                if (e.deltaY > 0) {
+                    this.move(this.nextBtn);
+                } else if (e.deltaY < 0) {
+                    this.move(this.prevBtn);
+                }
+                this.flag = false;
+                setTimeout(() => {
+                    this.flag = true;
+                }, 1000);
+            }
+        })
+        /*======================================================= /wheel =========================================================*/
+
+
+        /*======================================================= keys ==========================================================*/
+
+        window.addEventListener('keydown', (e) => {
+            if (this.flag === true) {
+                if (e.code == 'ArrowDown') {
+                    this.move(this.nextBtn);
+                } else if (e.code == 'ArrowUp') {
+                    this.move(this.prevBtn);
+                }
+                this.flag = false;
+                setTimeout(() => {
+                    this.flag = true;
+                }, 1000);
+            }
+        })
+        /*======================================================= /keys ==========================================================*/
+
+        this.touchStartY;
+        /*======================================================= swipe =========================================================*/
+        this.pages[this.activePage].addEventListener('touchstart', (e) => {
+            this.touchStartY = e.touches[0].clientY;
+            console.log(this.touchStartY);
+        })
+        this.pages[this.activePage].addEventListener('touchmove', (e) => {
+            this.change = e.touches[0].clientY - this.touchStartY;
+            console.log(this.change);
+            this.pages[this.activePage].style.transform = `translateY(${this.change}px)`
+            if (this.change > 0) {
+                this.pages[this.activePage - 1].style.transform = `translateY(${this.pageHeight - this.change}px)`
+            } else if (this.change < 0) {
+                this.pages[this.activePage - 1].style.transform = `translateY(${this.pageHeight - this.change}px)`
+            } else {
+                console.log('no change');
+            }
+        })
+        /*======================================================= /swipe =========================================================*/
 
     }
 
@@ -55,11 +140,7 @@ class Slider {
             this.pages[this.activePage].style.transition = `1s`;
             this.pages[this.activePage].style.transform = `translateY(0)`;
             this.pages[this.activePage].classList.add('active');
-            this.prevBtn.disabled = true;
             this.checkIndicator();
-            setTimeout(() => {
-                this.prevBtn.disabled = false;
-            }, 1000);
         } else if (btn == this.nextBtn && this.activePage < this.pages.length - 1) {
             this.pages[this.activePage].classList.remove('active');
             this.pages[this.activePage].style.transition = `1s`;
@@ -72,11 +153,7 @@ class Slider {
             this.pages[this.activePage].style.transition = `1s`;
             this.pages[this.activePage].style.transform = `translateY(0)`;
             this.pages[this.activePage].classList.add('active');
-            this.nextBtn.disabled = true;
             this.checkIndicator();
-            setTimeout(() => {
-                this.nextBtn.disabled = false;
-            }, 1000);
         }
 
     }
@@ -86,6 +163,27 @@ class Slider {
             this.indicators[i].classList.remove('active');
         }
         this.indicators[this.activePage].classList.add('active');
+    }
+    disableBtns(checker) {
+        if (checker == true) {
+            this.nextBtn.disabled = true;
+            this.nextBtn.disabled = true;
+        } else {
+            this.nextBtn.disabled = false;
+            this.nextBtn.disabled = false;
+        }
+
+    }
+    disableIndicators(checker) {
+        if (checker == true) {
+            for (let i = 0; i < this.indicators.length; i++) {
+                this.indicators[i].disabled = true;
+            }
+        } else {
+            for (let i = 0; i < this.indicators.length; i++) {
+                this.indicators[i].disabled = false;
+            }
+        }
     }
 
 }
